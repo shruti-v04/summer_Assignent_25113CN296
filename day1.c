34dc471dfe5058817ff26158d71be1c12248630b
shruti-v04 <v.shrutii.1201@gmail.com>
2026-05-29 16:14:39 +0530

    second commit

--- day1.c
+++ day1.c
@@ -1,3 +1,6 @@
+/*1.Write a program to Calculate sum of first N 
+natural numbers. */
+
 #include<stdio.h>
 int main()
 {
@@ -8,5 +11,40 @@ int main()
         sum+=i;
     }       
     printf("the sum of first %d natural numbers is %d",n,sum);
-    return 0;   
+      
+
+/*2.Write a program to Print multiplication table of 
+a given number.*/
+
+    int num;
+    printf("\n enter the number:");
+    scanf("%d",&num);
+    for(int i=1;i<=10;i++){
+        printf("%d x %d = %d\n",num,i,num*i);
+    }
+    
+/* 3. Write a program to Find factorial of a number. */
+
+    int fact=1;
+    int nm;
+    printf("\n enter the number:");
+    scanf("%d",&nm);
+    for(int i=1;i<=nm;i++){
+        fact*=i;
+    }
+    printf("the factorial of %d is %d",nm,fact);
+
+/* 4.  Write a program to Count digits in a number.*/
+
+    int count=0;
+    int number;
+    printf("\n enter the number:");
+    scanf("%d",&number);
+    while(number!=0){
+        number/=10;
+        count++;
+    }
+    printf("the number of digits in the given number is %d",count);
+    return 0;
+    
 }

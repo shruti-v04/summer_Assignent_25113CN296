1ea4783750937bf3ab78308916d2d90b6ec9ef60
Shruti Verma <v.shrutii.1201@gmail.com>
2026-06-19 18:53:28 +0530

    commit

---
+++ DAY 18 /Q72)sortdesecending.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Sorting in descending order
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] < arr[j]) {
+                swap(arr[i], arr[j]);
+            }
+        }
+    }
+
+    cout << "Array in descending order:\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
